299efde4cdabe424795b007fa1e3bfe9b17eb541
孟容聖 <synz00023@gmail.com>
2023-05-17 23:05:05 +0800

    樣板引擎

--- project/flask_training/app.py
+++ project/flask_training/app.py
@@ -1,23 +1,26 @@
 # 引入 flask 裡的 Flask
-from flask import Flask
+from flask import Flask, render_template
 # --------------------------建立 Flask 物件----------------------------------
 app=Flask(__name__) # 參數: 應用程式的名子
 app.debug=True
 # -----------------------------設置路由--------------------------------------
-# def index():
-#     return "welcome to here!"
-# app.add_url_rule("/", "index", index)
-# 和下面裝飾器寫法相同
+# -----------------首頁------------------
 @app.route("/")
 @app.route("/index.html")
 def index():
-    return "首頁"
-@app.route("/about")
-def about():
-    return "關於我們"
-@app.route("/faq/")
-def faq():
-    return "問答集"
-# -----------------------------啟動伺服器-----------------------------------
+    return app.send_static_file("index.html")
+# ---------------關於使用者---------------
+@app.route("/about/<user>/")
+def about(user):
+    return render_template("index.html", name=user)
+# ---------------成績發表-----------------
+@app.route("/test/<int:score>/")
+def test(score):
+    return render_template("score.html", score=score)
+# -----------------訂單------------------
+@app.route("/order/")
+def order():
+    return render_template("order.html")
+# -----------------------------啟動伺服器------------------------------------
 if __name__=="__main__":
-    app.run()
+    app.run("0.0.0.0", 80)
